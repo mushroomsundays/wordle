50e59998acff5e81d7accbc3c1a042b57b8d1a29
John <base_1187@yahoo.com>
2022-02-19 19:19:14 -0500

    added used word list to gitignore

---
+++ TODO.py
@@ -0,0 +1,6 @@
+x. Game over after 6 guesses
+2. Store user scores in a json file
+x. Allow user to give up and see the secret_word
+x. Create used_words.txt and filter them out
+5. Write tests
+6. Make solver
